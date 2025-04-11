71b6ef9c4685d1784a9be270eb8fec2d1637e4b1
Nicolas Gomez <nicolasgomez94@gmail.com>
2025-04-11 17:41:38 -0300

    import React from "react"; en TODOS

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import AdminLogin from "./AdminLogin";
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 const Success = () => (
 	<div style={{ textAlign: "center",
 			color: "white",
