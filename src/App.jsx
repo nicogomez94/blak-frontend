9e95d8132f718793a3aea32a986551d878444462
Nicolas Gomez <nicolasgomez94@gmail.com>
2025-04-11 17:43:41 -0300

    correcion import React from "react";

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CalendarComponent from "./components/CalendarComponent";
